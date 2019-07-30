bc2b897e2d38c15a06ca8762272b25e6a4f1c471
Timothy Putzke <tsputzke@gmail.com>
2019-07-30 19:24:17 -0400

    Add cors, helmet

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const POKEDEX = require('./pokedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
